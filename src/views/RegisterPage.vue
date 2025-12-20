53919baaaf683af7a985d80a574e5408c1d3b69d
[dyad] <gabrielmcoenga@gmail.com>
2025-12-19 22:17:34 -0400

    [dyad] Removida a referência a uma imagem de fundo ausente em RegisterPage.vue para corrigir o erro de build no GitHub Pages. - wrote 1 file(s)

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -291,7 +291,7 @@ const goToLogin = () => {
   left: 0;
   right: 0;
   bottom: 0;
-  background-image: url('@/assets/backgrounds/dark-red-2.jpg');
+  /* Removido: background-image: url('@/assets/backgrounds/dark-red-2.jpg'); */
   background-size: cover;
   background-position: center;
   opacity: 0.1;
